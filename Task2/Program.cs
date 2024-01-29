4fad199ad051f4120fc9b72108d545f019c919eb
evgen <evgen0404@mail.ru>
2024-01-29 20:01:02 +0300

    add Lesson6:Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main() 
+    {
+        // Строка, содержащая латинские буквы в обоих регистрах.
+        string str = "JsddKDjk1-";
+
+        // Преобразование всех заглавных букв в строчные 
+        string result = str.ToLower();
+
+        // Вывод результата
+        Console.WriteLine(result);
+
+    }
+}
